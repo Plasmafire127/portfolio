36609f4c92efe913647d3a76bc4e96833e553879
Jake <jdigiug1@binghamton.edu>
2024-04-08 02:44:54 -0400

    added 404 handling

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,17 @@ const dev = process.argv.includes('dev');
 const config = {
     preprocess: vitePreprocess(),
     kit: {
+      prerender: {
+        handleHttpError: ({ path, referrer, message }) => {
+          // ignore deliberate link to shiny 404 page
+          if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+            return;
+          }
+  
+          // otherwise fail the build
+          throw new Error(message);
+        }
+      },
     adapter: adapter(),
     paths: {
       base: dev ? '' : process.env.BASE_PATH,
